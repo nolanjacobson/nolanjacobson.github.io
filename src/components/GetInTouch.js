22f5ae369cec7b983b72cbc8afab1768003a0666
Nolan Jacobson <nolanjacobsonwork@Nolans-MacBook-Pro.local>
2023-02-11 10:32:58 -0500

    changes

---
+++ src/components/GetInTouch.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+const FORM_ENDPOINT = "https://formspree.io/f/xdopbwyb"; // TODO - fill on the later step
+
+const GetInTouch = () => {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+        setTimeout(() => {
+            setSubmitted(true);
+        }, 100);
+    };
+
+    if (submitted) {
+        return (
+            <>
+                <div className="text-2xl">Thank you!</div>
+                <div className="text-md">We'll be in touch soon.</div>
+            </>
+        );
+    }
+
+    return (
+        <section id="about">
+            <div className="col-md-12">
+                <h1 className="section-title">Get in Touch with me at nolanjacobson@protonmail.com</h1>
+                {/* <form
+                action={FORM_ENDPOINT}
+                onSubmit={handleSubmit}
+                method="POST"
+                target="_blank"
+                className="form-center"
+            >
+                <div className="mb-3 pt-0">
+                    <input
+                        type="text"
+                        placeholder="Your name"
+                        name="name"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                        required
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                        required
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <textarea
+                        placeholder="Your message"
+                        name="message"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                        required
+                    />
+                </div>
+                <div className="mb-3 pt-0">
+                    <button
+                        className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="submit"
+                    >
+                        Send a message
+                    </button>
+                </div>
+            </form> */}
+            </div>
+        </section>
+    );
+};
+
+export default GetInTouch;
