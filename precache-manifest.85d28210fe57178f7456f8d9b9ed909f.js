0b19f83069a597eea6e662fe8de0907733020372
Nolan Jacobson <nolanjacobsonwork@Nolans-MacBook-Pro.local>
2023-02-11 10:51:15 -0500

    Updates

---
+++ precache-manifest.85d28210fe57178f7456f8d9b9ed909f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4455e4688c6d79e844237f76f2caed4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a82f7c482582876ad05",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "23025241f6bd7c976457",
+    "url": "/static/css/main.9655f206.chunk.css"
+  },
+  {
+    "revision": "2a82f7c482582876ad05",
+    "url": "/static/js/2.c67c3979.chunk.js"
+  },
+  {
+    "revision": "eda0ffd0004ecfd7814ea7afb2792755",
+    "url": "/static/js/2.c67c3979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23025241f6bd7c976457",
+    "url": "/static/js/main.df42dfb9.chunk.js"
+  },
+  {
+    "revision": "43040981eeb9a21a465f",
+    "url": "/static/js/runtime-main.60683f2f.js"
+  },
+  {
+    "revision": "ae61b88b9ea90e7cf85d35dc9181d4ef",
+    "url": "/static/media/autographlogo.ae61b88b.jpeg"
+  },
+  {
+    "revision": "cb65fdcd3787bd4713a87b6ab0137442",
+    "url": "/static/media/chainlink.cb65fdcd.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee51f0585aaf7f45d1b9d9d5b4f40f3b",
+    "url": "/static/media/nearprotocol.ee51f058.png"
+  },
+  {
+    "revision": "e758099694b91e729a40241cb214377e",
+    "url": "/static/media/nolanjacobson.e7580996.jpeg"
+  },
+  {
+    "revision": "1eb2d68a58375a2fc07d74c743e21dc2",
+    "url": "/static/media/qtum-ethers.1eb2d68a.png"
+  },
+  {
+    "revision": "04ac7480aa2b04637b614f1e520fc508",
+    "url": "/static/media/qtum.04ac7480.png"
+  },
+  {
+    "revision": "3679a8e09e41a8f8a61c168cf89e0054",
+    "url": "/static/media/qtumllogo.3679a8e0.png"
+  },
+  {
+    "revision": "64a3d21e08f664a4bc2f64f2657be24a",
+    "url": "/static/media/tbx.64a3d21e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nolanjacobson.github.io/precache-manifest.1039f12b986a4e7aeb5fae1d97e22ea6.js"
+  "/precache-manifest.85d28210fe57178f7456f8d9b9ed909f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nolanjacobson.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
