60eb67807257f08f12914bed592ed5bb6cd143dd
Nolan Jacobson <nolanjacobsonwork@Nolans-MacBook-Pro.local>
2023-02-11 10:48:53 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nolanjacobson/precache-manifest.551b6a6fc68f11240476476280574504.js"
+  "/nolanjacobson.github.io/precache-manifest.1039f12b986a4e7aeb5fae1d97e22ea6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nolanjacobson/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nolanjacobson.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
